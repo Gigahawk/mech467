15668fdd3d0c5b69d8f84d0fc3b9b1c982a59ca2
Jasper Chan <jasperchan515@gmail.com>
2020-11-23 16:08:27 -0800

    Prelab 3: Update Spongebob toolpath to include shoes

--- Labs/Lab 3/prelab/custom_toolpath/abs_to_inc.py
+++ Labs/Lab 3/prelab/custom_toolpath/abs_to_inc.py
@@ -1,20 +1,27 @@
+from decimal import *
+getcontext().prec = 100000
+
 with open('./spongebob.gcode', 'r') as f:
     lines = f.readlines()
 
-last_x = 0
-last_y = 0
+
+last_x = Decimal(0)
+last_y = Decimal(0)
 out = []
 for l in lines:
+    print(f"orig: {l}")
     l = l.split()
-    x = float(l[1][1:])
-    y = float(l[2][1:])
+    x = Decimal(l[1][1:])
+    y = Decimal(l[2][1:])
     dx = x - last_x
     dy = y - last_y
     last_x = x
     last_y = y
+    # Necessary to fix weird drift issue
+    last_y += Decimal(0.0001)
     nl = f"G1 X{dx} Y{dy}\n"
     out.append(nl)
-    print(nl)
+    print(f"newo: {nl}")
 
 out = out[:-1]
 
